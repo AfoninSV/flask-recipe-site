746846a1921dccb9cb82785b136f22f8fc249198
Serhii Afonin <serhij.afonin@gmail.com>
2023-10-01 15:32:02 -0300

    added user, meal models

--- web_meal/db_models.py
+++ web_meal/db_models.py
@@ -1,20 +1,28 @@
-import peewee as pw
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
-database = pw.SqliteDatabase("database.db")
+from typing import List
 
-
-class BaseModel(pw.Model):
-    id = pw.AutoField()
-
-    class Meta:
-        database = database
+class Base(DeclarativeBase):
+    pass
 
 
-class User(BaseModel):
-    user_id = pw.TextField()
+class User(Base):
+    __tablename__ = "user"
+    user_id = mapped_column(String(30))
+    favorites = relationship(List["Meal"])
+
+    def __repr__(self):
+        return f"User (user_id={self.user_id})"
 
 
-database.user = User
-
-database.connect()
-database.create_tables([User])
+class Meal(Base):
+    __tablename__ = "meal"
+    id = mapped_column(Integer, primary_key=True)
+    meal_id = mapped_column(String(10))
+    title = mapped_column(String(100))
+    ingredients = mapped_column(String, nullable=True)
+    ingredients_qty = mapped_column(Integer, nullable=True)
+    instruction = mapped_column(String)
+    picture_link = mapped_column(String)
+    video_link = mapped_column(String, nullable=True)
