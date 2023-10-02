b7d1cd60ea2253807419cb3e4676bfb2ae7eecd1
Serhii Afonin <serhij.afonin@gmail.com>
2023-10-02 13:59:31 -0300

    finalized user/meal tables

--- web_meal/db_models.py
+++ web_meal/db_models.py
@@ -1,7 +1,6 @@
-from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy import Integer, String, Text
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
-from typing import List
 
 class Base(DeclarativeBase):
     pass
@@ -9,8 +8,9 @@ class Base(DeclarativeBase):
 
 class User(Base):
     __tablename__ = "user"
-    user_id = mapped_column(String(30))
-    favorites = relationship(List["Meal"])
+    user_id = mapped_column(String(30), primary_key=True)
+    favorites: Mapped[list["Meal"]] = relationship(
+        primaryjoin='foreign(Meal.meal_id) == User.user_id')
 
     def __repr__(self):
         return f"User (user_id={self.user_id})"
@@ -21,8 +21,8 @@ class Meal(Base):
     id = mapped_column(Integer, primary_key=True)
     meal_id = mapped_column(String(10))
     title = mapped_column(String(100))
-    ingredients = mapped_column(String, nullable=True)
+    ingredients = mapped_column(Text, nullable=True)
     ingredients_qty = mapped_column(Integer, nullable=True)
-    instruction = mapped_column(String)
-    picture_link = mapped_column(String)
-    video_link = mapped_column(String, nullable=True)
+    instruction = mapped_column(Text)
+    picture_link = mapped_column(String(255))
+    video_link = mapped_column(String(255), nullable=True)
